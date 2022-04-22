5a44093a6967a4e12718b590a3dfbbade111cc68
Rabiul Hasan <34013030+robi042@users.noreply.github.com>
2022-04-22 23:02:04 +0600

    make it better

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000    
+const port = process.env.PORT || 3000    
 app.use(express.static(__dirname + '/protfolio'));
 app.use(express.static(__dirname + '/love/dist'));
+app.get('/', (req, res) => { 
+  res.send("hello!")
+})
 
 app.get('/prome', (req, res) => { 
     res.sendFile('index.html', { root:__dirname + '/love/dist'})
